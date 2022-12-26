5f25b92d76f7142212bf335bee5920b1ec195c07
werble7 <60023966+werble7@users.noreply.github.com>
2022-12-26 18:38:20 -0300

    Adicionando o esboco
    Arquivos copiados da aula introdutória de sockets

---
+++ cliente.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 50000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall("Bom dia!".encode())
+data = s.recv(1024)
+
+print('mensagem ecoada:', data.decode())
+
---
+++ servidor.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = 'localhost'
+PORT = 50000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+
+print("Aguardando conexão de um cliente")
+
+conn, ender = s.accept()
+print("Conectado em", ender)
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        print("Fechando a conexão")
+        conn.close()
+        break
+
+    conn.sendall(data)
+    
